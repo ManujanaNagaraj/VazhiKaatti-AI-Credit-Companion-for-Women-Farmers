d99731a3661d23b42c3a7b3ccac8f2d7226bfdf9
﻿Manujana Nagaraj <manunagaraj87@gmail.com>
2026-03-06 14:32:13 +0530

    Add AnimatePresence and route transitions to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
 import FarmerLogin from './pages/FarmerLogin';
@@ -9,20 +10,30 @@ import CreditScore from './pages/CreditScore';
 import SchemeMatcher from './pages/SchemeMatcher';
 import OfficerDashboard from './pages/OfficerDashboard';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+  
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<FarmerLogin />} />
+        <Route path="/profile" element={<FarmerProfile />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/score" element={<CreditScore />} />
+        <Route path="/schemes" element={<SchemeMatcher />} />
+        <Route path="/officer" element={<OfficerDashboard />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<FarmerLogin />} />
-          <Route path="/profile" element={<FarmerProfile />} />
-          <Route path="/questions" element={<Questions />} />
-          <Route path="/score" element={<CreditScore />} />
-          <Route path="/schemes" element={<SchemeMatcher />} />
-          <Route path="/officer" element={<OfficerDashboard />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
